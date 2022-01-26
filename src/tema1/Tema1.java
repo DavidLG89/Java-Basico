023d9f113fa69dd553acb89ca44438bcb670e923
David Ramon Thomen <thomendavid1@gmail.com>
2022-01-25 21:01:17 -0300

    Primer commit

---
+++ src/tema1/Tema1.java
@@ -0,0 +1,32 @@
+package tema1;
+
+public class Tema1 {
+
+    public static void main(String[] args) {
+
+        // 1. Datos numéricos
+        // 1.1 Enteros
+        byte variable1 = 3;
+        System.out.println(variable1);
+        short variable2 = 45;
+        int variable3 = 23;
+        long variable4 = 345;
+        long numeroTelefono = 23452453;
+        // long precio = null;
+
+        // 1.2 decimales
+        float variable5 = 2.2f;
+        double variable6 = 10.5d;
+        variable6 = 20.5d;
+        double precioSilla = 49.99;
+
+        // 2. booleano
+        boolean variable7 = false;
+        boolean variable8 = true;
+
+        // 3. texto
+        char variable9 = 'z';
+        String variable10 = "Cualquier texto";
+    }
+
+}
